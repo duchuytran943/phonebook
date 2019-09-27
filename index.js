45f9dce0c901b08889f25216035a4da1983ff5ba
Duc Huy Tran <duchuytran943@gmail.com>
2019-09-27 16:32:56 +0700

    add index.js and data.json
    menu function in index.js
    and data save to data.json
    
    Signed-off-by: Duc Huy Tran <duchuytran943@gmail.com>

---
+++ index.js
@@ -0,0 +1,23 @@
+
+/**
+ * Sử dụng kiến thức đã học, tạo ra một ứng dụng danh bạ điện thoại, có các chức năng:
+ * - Nhập dữ liệu contact (name, phone number)
+ * - Sửa dữ liệu contact
+ * - Xoá contact
+ * - Tìm kiếm contact: có thể nhập vào tên (không dấu hoặc có dấu, chữ hoa hoặc chữ thường vẫn cho ra kết quả) hoặc 1 phần số điện thoại
+ */
+
+function showMenu() {
+    console.log('0. Show all contact');
+    console.log('1. Create new contact');
+    console.log('2. Edit contact');
+    console.log('3. Erase contact ');
+    console.log('4. Search contact');
+
+}
+function main() {
+    showMenu();
+}
+
+main();
+
