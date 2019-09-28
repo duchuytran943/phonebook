a883bf7e14aeb41e9f5c14d5081ec1acccd60e91
Duc Huy Tran <duchuytran943@gmail.com>
2019-09-29 06:04:10 +0700

    function searchContact()
    Signed-off-by: Duc Huy Tran <duchuytran943@gmail.com>

--- index.js
+++ index.js
@@ -60,6 +60,63 @@ function editContact() {
     // console.log(oldContact);
 }
 
+function searchContact() {
+    var valSearch = readlineSync.question('> Enter name or phone: ' );
+    search(contacts, valSearch);
+    showMenu();
+}
+function search(ob, q) {
+    if (!isNaN(q)) {
+        q = Number(q);
+        var arr = new Array();
+        for (x of ob) {
+            if (Number(x.phone).toString().indexOf(Number(q).toString()) >= 0) {
+
+                arr.push(x);
+
+            }
+        }
+        showContacts(arr);
+    } else {
+        q = q.toString();
+        let arr = new Array();
+        for (x of ob) {
+            if (change_alias(x.name).toLowerCase().indexOf(change_alias(q).toLowerCase()) >= 0) {
+                arr.push(x);
+            }
+        }
+        showContacts(arr);
+    }
+
+}
+function change_alias(alias) {
+    var str = alias;
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+    str = str.replace(/đ/g, "d");
+    str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+    str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+    str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+    str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+    str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+    str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+    str = str.replace(/Đ/g, "D");
+    str = str.trim();
+    return str;
+}
+
+function showContacts(ob) {
+    console.log('============= SEARCH RESULTS =============');
+    for (x of ob) {
+        console.log("id: " + x.id + " || name: " + x.name + " || phone: " + x.phone);
+    }
+}
+
+
 function showMenu() {
     console.log('==================================================');
     console.log('1. Show all contact');
